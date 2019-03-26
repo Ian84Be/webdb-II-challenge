837ccd430d6f68ceb015886083780d25f16899ad
Ian Be <ian84be@gmail.com>
2019-03-26 16:31:18 -0500

    STRETCH: add bears table & bears-router.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const helmet = require('helmet');
 
 const server = express();
 const zoosRouter = require('./zoos-router.js');
+const bearsRouter = require('./bears-router.js');
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
 server.use('/api/zoos', zoosRouter);
+server.use('/api/bears', bearsRouter);
 
 const port = 5000;
 server.listen(port, function() {
